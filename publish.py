a72145c7120eca47818f9ef3770b6e21f660d498
pablo veron <pveron@upgrade.com>
2018-01-13 10:04:36 -0300

    Firs Commit

---
+++ publish.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
+import sys
+
+mqttc = mqtt.Client("python_pub")
+mqttc.connect("localhost", 1883)
+mqttc.publish("broker/messages", sys.argv[1])
+mqttc.loop(2) #timeout = 2s
